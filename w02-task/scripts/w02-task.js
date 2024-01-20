5501947d8b3add3707efa78b57a98b4ed4329290
AlexMyers2000 <104384449+AlexMyers2000@users.noreply.github.com>
2024-01-20 20:42:07 +0000

    week 2

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,32 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname = 'Alex Myers';
+// Declare and instantiate a variable for the current year
+let currentYear = new Date().getFullYear();
+
+// Declare and instantiate a variable for the profile picture file path
+let profilePicture = "images/IMG_3871.JPG";
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
